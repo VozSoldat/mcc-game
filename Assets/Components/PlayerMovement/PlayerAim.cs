41a404f74445c5d5fde3cd9f59d3226536874464
VozSoldat <muhamaderril7@gmail.com>
2025-01-24 14:42:03 +0700

    ✨ Add raycasting
    for mouse cursor's position in the worl
    
    for player view and bullet trajectory

---
+++ Assets/Components/PlayerMovement/PlayerAim.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    private MouseCursor mouseCursor;
+    public Vector3 LookDirection { get; set; }
+    public LayerMask layerMask;
+    private void Start()
+    {
+        mouseCursor = GameObject.Find("GameManager").GetComponent<MouseCursor>();
+    }
+
+    private void Update()
+    {
+        Ray ray = new Ray(transform.position, mouseCursor.Position - transform.position); // Hitung arah ray
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, layerMask)) // Tambahkan layerMask di sini
+        {
+            LookDirection = hit.point;
+            Debug.Log("Kursor 2 mengenai: " + hit.collider.name);
+        }
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+    }
+}
