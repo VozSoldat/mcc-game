f35330a2e2dd6eea743f3652e1b2930e62b959c5
VozSoldat <muhamaderril7@gmail.com>
2025-01-25 10:40:56 +0700

    feat: Add bound distance

--- Assets/Components/Combat/Balistics/Bullet.cs
+++ Assets/Components/Combat/Balistics/Bullet.cs
@@ -3,12 +3,28 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Vector3 playerPosition;
+    [SerializeField] private float boundDistance = 50f;
     [SerializeField] private int physicalDamage = 1;
     [SerializeField] private int acidDamage = -1;
     [SerializeField] private float bulletSpeed = 50;
     public float Speed { get { return bulletSpeed; } set { bulletSpeed = value; } }
     public int PhysicalDamage { get => physicalDamage; set => physicalDamage = value; }
     public int AcidDamage { get => acidDamage; set => acidDamage = Math.Clamp(value, -7, 7); }
+
+    private void Start()
+    {
+        playerPosition = GameObject.Find("Player").transform.position;
+    }
+
+    private void Update()
+    {
+        
+        if ((playerPosition - transform.position).magnitude > boundDistance)
+        {
+            DestroySelf();
+        }
+    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -18,6 +34,10 @@ public class Bullet : MonoBehaviour
             target.ReceiveDamage(this.gameObject);
         }
 
-        Destroy(this.gameObject);
+        DestroySelf();
+    }
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
     }
 }
