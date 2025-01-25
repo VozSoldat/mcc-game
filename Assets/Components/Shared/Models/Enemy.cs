a66664668f689f1238912841993f60dd5554dcff
VozSoldat <muhamaderril7@gmail.com>
2025-01-25 09:04:21 +0700

    Ref: rename field and property of acid health
    - rename `acidHealth` to `acidity`
    - rename `AcidHealth` to `Acidity`

--- Assets/Components/Shared/Models/Enemy.cs
+++ Assets/Components/Shared/Models/Enemy.cs
@@ -5,8 +5,8 @@ public class Enemy : MonoBehaviour, IEntity
 {
     [SerializeField] private float movementSpeed = 100;
     // [SerializeField] private int health = 0;
-    [SerializeField] private int acidHealth = 0;
+    [SerializeField] private int acidity = 0;
     public float MovementSpeed { get => movementSpeed; set => movementSpeed = value; }
-    public int AcidHealth { get => acidHealth; set=> acidHealth = Math.Clamp(value, -7, 7); }
+    public int Acidity { get => acidity; set=> acidity = Math.Clamp(value, -7, 7); }
     // public int Health { get => health; set => health = Math.Clamp(value, 0, 100); }
 }
