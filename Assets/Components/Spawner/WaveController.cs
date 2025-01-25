a183025e868ca28709617845da33111a8fc212f0
VozSoldat <muhamaderril7@gmail.com>
2025-01-25 14:45:18 +0700

    feat: Add wave and spawn system

---
+++ Assets/Components/Spawner/WaveController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    [SerializeField] private int currentWave = 0;
+    public int CurrentWave => currentWave;
+    public float spawnDelay = 3;
+
+    private void Update()
+    {
+        // Debug.Log("Wave: " + currentWave + " Enemies: " + WorldEnemyCounter());
+
+        if (WorldEnemyCounter() <= 0)
+        {
+            // currentWave++;
+            StartCoroutine(DelayWaveChange());
+
+        }
+    }
+    private int WorldEnemyCounter()
+    {
+        return GameObject.FindGameObjectsWithTag("Enemy").Length;
+    }
+
+    IEnumerator DelayWaveChange()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+
+        currentWave++;
+
+    }
+}
