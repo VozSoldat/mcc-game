44cd41ff7283c29f8c0e9247e8b697dc12a7c8b5
alimulap <mohammad4limul@gmail.com>
2025-01-25 12:37:46 +0700

    feat: Enemy shooting bullets

--- Assets/Components/Combat/Balistics/Bullet.cs
+++ Assets/Components/Combat/Balistics/Bullet.cs
@@ -3,14 +3,34 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private int physicalDamage = 1;
-    [SerializeField] private int acidDamage = -1;
-    [SerializeField] private float bulletSpeed = 50;
-    public float Speed { get { return bulletSpeed; } set { bulletSpeed = value; } }
-    public int PhysicalDamage { get => physicalDamage; set => physicalDamage = value; }
-    public int AcidDamage { get => acidDamage; set => acidDamage = Math.Clamp(value, -7, 7); }
+    [SerializeField]
+    private int physicalDamage = 1;
 
-    private void OnCollisionEnter(Collision other)
+    [SerializeField]
+    private int acidDamage = -1;
+
+    [SerializeField]
+    private float bulletSpeed = 50;
+
+    public String spawnerTag;
+
+    public float Speed
+    {
+        get { return bulletSpeed; }
+        set { bulletSpeed = value; }
+    }
+    public int PhysicalDamage
+    {
+        get => physicalDamage;
+        set => physicalDamage = value;
+    }
+    public int AcidDamage
+    {
+        get => acidDamage;
+        set => acidDamage = Math.Clamp(value, -7, 7);
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         var target = other.gameObject.GetComponent<IEntityHit>();
         if (target != null)
@@ -18,6 +38,10 @@ public class Bullet : MonoBehaviour
             target.ReceiveDamage(this.gameObject);
         }
 
-        Destroy(this.gameObject);
+        if (!other.CompareTag(this.spawnerTag))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
+
